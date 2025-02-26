15acecf9f85df058790a4f5984bc0d6269b058bb
Erik <erik@gimmen.se>
2025-02-26 14:35:46 +0100

    part 1 b

--- newton.hs
+++ newton.hs
@@ -7,19 +7,29 @@ import Prelude hiding (  (+), (-), (*), (/), negate, recip, (^),
 type Tri a = (a, a, a)
 
 instance Additive a => Additive (Tri a) where
-        (+) = error "TODO"
-        zero = error "TODO"
-instance Additive a => AddGroup (Tri a) where
-        negate = error "TODO"
-instance Multiplicative a => Multiplicative (Tri a) where
-        (*) = error "TODO"
-        one = error "TODO"
-instance (Additive a, Multiplicative a) => MulGroup (Tri a) where
-        recip = error "TODO"
+        (+) = addTri
+        zero = (zero, zero, zero)
+addTri :: Additive a => Tri a -> Tri a -> Tri a
+addTri (a,b,c) (x,y,z) = (a+x,b+y,c+z)
+
+instance AddGroup a => AddGroup (Tri a) where
+        negate (a,b,c) = (negate a, negate b, negate c)
+
+instance (Additive a, Multiplicative a) => Multiplicative (Tri a) where
+        (*) = mulTri -- TODO: Maybe not how we're supposed to multiply triples
+        one = (one, one, one)
+mulTri :: (Multiplicative a, Additive a) => Tri a -> Tri a -> Tri a
+mulTri (a,b,c) (x,y,z) = (a*x,b*y,c*z)
+
+instance (AddGroup a, MulGroup a) => MulGroup (Tri a) where
+        recip (a,b,c) = (recip a, recip b, recip c)
 
 instance Transcendental a => Transcendental (Tri a) where
-        pi = error "TODO"
-        sin = error "TODO"
-        cos = error "TODO"
-        exp = error "TODO"
+        pi = (pi, pi, pi)
+        sin (a,b,c) = (sin a, sin b, sin c)
+        cos (a,b,c) = (cos a, cos b, cos c)
+        exp (a,b,c) = (exp a, exp b, exp c)
 
+-- PART 1 
+-- C
+evalDD = error "TODO"
